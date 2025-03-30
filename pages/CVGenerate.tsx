6f29dd4a23ebde275ccd4123e1f1fa7195ae1d31
TinMH <tinhongmai1012@gmail.com>
2025-03-30 22:07:52 +0700

    Create Nvbar

---
+++ pages/CVGenerate.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import Nvbar from '../component/Nvbar'
+import { BrowserRouter as Router } from 'react-router-dom';
+
+const Home = () => {
+  return (
+    <div>
+        Generate CV
+    </div>
+  )
+}
+
+export default Home
---
+++ pages/FAQ.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import Nvbar from '../component/Nvbar'
+import { BrowserRouter as Router } from 'react-router-dom';
+
+const Home = () => {
+  return (
+    <div>
+        FAQ
+    </div>
+  )
+}
+
+export default Home
